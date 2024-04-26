69bfefe9121c45cd2e3635edb1c6257933fb665e
MrM <mohamadokazemi2@gmail.com>
2024-04-27 03:00:25 +0330

    make Data_base Class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Data_base
+{
+    // make Data_base class way can use it for inhirate
+    protected:
+        vector<string> Username;
+        vector<string> Pass;
+        vector<string> collegian;
+        vector<int>    collegian_list;
+        vector<string> Descriptive_questions;
+        vector<string> Descriptive_answer;
+        vector<string> Multiplechoice_questions;
+        vector<char>   Multiplechoice_answer;
+        vector<double> collegian_point;
+        vector<string> discription_descrptive;
+        vector<string> discription_multiplechoice;
+    public:
+        void Data_base() {
+            //make space by constactor
+        };
+};
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
