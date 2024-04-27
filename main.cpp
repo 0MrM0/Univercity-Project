711318260ca2a0d270b02f288e380a869be38023
MrM <mohamadokazemi2@gmail.com>
2024-04-27 11:21:53 +0330

    include fstream for work file(for collegian list maker easy) and add 1 classininhirate from Data_base make better 3 class(Data_base, Teacher, Collegian) and now try to fix some bugs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
@@ -7,15 +8,18 @@ using namespace std;
 class Data_base
 {
     // make Data_base class way can use it for inhirate
+    private:
+        fstream cl0("collegianlist0.txt");
+        fstream cl1("collegianlist1.txt");
+        fstream cl2("collegianlist2.txt");
+        fstream cl3("collegianlist3.txt");
+        fstream cl4("collegianlist4.txt");
+        fstream cl5("collegianlist5.txt");
     protected:
         vector<string> Username;
         vector<string> Pass;
-        vector<string> collegian;
-        vector<int>    collegian_list;
-        vector<string> Descriptive_questions;
-        vector<string> Descriptive_answer;
-        vector<string> Multiplechoice_questions;
-        vector<char>   Multiplechoice_answer;
+        vector<int>    collegian_list = {0, 1, 2, 3, 4 ,5}; //techer just can make 5 list
+        //0 in collegian_list for all list,other customize
         vector<double> collegian_point;
         vector<string> discription_descrptive;
         vector<string> discription_multiplechoice;
@@ -26,14 +30,24 @@ class Data_base
 };
 
 class Teacher : public Data_base {
+    protected:
+        vector<string> Descriptive_questions;
+        vector<string> Descriptive_answer;
+        vector<string> Multiplechoice_questions;
+        vector<char>   Multiplechoice_answer;
     public:
-        Teacher() {
+        Teacher()
+        {
+            Username.push_back("Dr.Lotfi");
         }
-        Username.push_back("DrLotfi");
+};
 
-
-
-
+class Collegian : public Teacher {
+    public:
+        vector<string> collegian;
+        Collegian() {
+            collegian.push_back(cl0);
+        }
 };
 
 int main()
