05e86bae2b308789de5a8aa4d6949b3c36ccfa49
MrM <mohamadokazemi2@gmail.com>
2024-05-09 10:25:54 +0330

    rebase from project github/push code and file to project

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
+#include <chrono>
+#include <ctime>
 #include <algorithm>
 
 using namespace std;
@@ -17,23 +20,38 @@ class Data_base
         vector<double> collegian_point;
         vector<string> discription_descrptive;
         vector<string> discription_multiplechoice;
+        vector<string> loginhistory;
     public:
         Data_base() {
             //make space by constructor & make some files use for collegian list
-            fstream cl0("C:\\path\\to\\collegianlist0.txt");
-            fstream cl1("C:\\path\\to\\collegianlist1.txt");
-            fstream cl2("C:\\path\\to\\collegianlist2.txt");
-            fstream cl3("C:\\path\\to\\collegianlist3.txt");
-            fstream cl4("C:\\path\\to\\collegianlist4.txt");
-            fstream cl5("C:\\path\\to\\collegianlist5.txt");
+            fstream cl0("C:\\path\\to\\collegianlist0.csv");
+            fstream cl1("C:\\path\\to\\collegianlist1.csv");
+            fstream cl2("C:\\path\\to\\collegianlist2.csv");
+            fstream cl3("C:\\path\\to\\collegianlist3.csv");
+            fstream cl4("C:\\path\\to\\collegianlist4.csv");
+            fstream cl5("C:\\path\\to\\collegianlist5.csv");
         }
         const string& getUser(size_t index) const {
             return User[index];
         }
-        const string& getpass(size_t index) const {
-            return Pass[index];
+        // const string& getPass(size_t index) const {
+        //     return Pass[index];
+        // }
+        // Function to find a user and return its index
+        int findUser(const string& username) const {
+            auto it = find(User.begin(), User.end(), username);
+            if (it != User.end()) {
+                return distance(User.begin(), it);
+            }
+            return -1; // Return -1 if user is not found
         }
-
+        // Function to get password using the index
+        const string& getPass(int index) const {
+                if (index >= 0 && index < Pass.size()) {
+                    return Pass[index];
+                }
+                throw out_of_range("Index out of range"); // Handle out-of-range errors
+        }
 };
 
 class Teacher : public Data_base {
@@ -52,7 +70,37 @@ class Teacher : public Data_base {
 
 class Collegian : public Teacher {
     public:
+        vector<string> collegian;
+        Collegian() {
+            //Read data for
+            ifstream file("C:\\path\\to\\collegianlist.csv");
+            string line;
+            while (getline(file, line)) {
+                stringstream ss(line);
+                string col1, col2;
+                getline(ss, col1, ','); // Get first column
+                getline(ss, col2, ','); // Get second column
+                // Remove spaces from col1 and col2 if necessary
+                col1.erase(remove(col1.begin(), col1.end(), ' '), col1.end());
+                col2.erase(remove(col2.begin(), col2.end(), ' '), col2.end());
+                // Concatenate col1 and col2 and push back into the vector
+                collegian.push_back(col1 + col2);
+            }
+            file.close();
 
+            ifstream collegianUser("C:\\path\\to\\collegianUser.csv"); //for now just me in the list
+            string collegianUserline;
+            while (getline(collegianUser, collegianUserline)) {
+                User.push_back(collegianUserline);
+            }
+            collegianUser.close();
+
+            ifstream collegianPass("C:\\path\\to\\collegianPass.csv"); //for now just me in the list
+            string collegianPassline;
+            while (getline(collegianPass, collegianPassline)) {
+                Pass.push_back(collegianPassline);
+            }
+            collegianPass.close();
         }
 };
 
@@ -110,17 +158,50 @@ bool T_authenticateUser(Teacher& Lotfi, int& Passcunter, string& username, strin
             cout << "Success Password is changed" << endl; // For test
             Passcunter = 3;
         }
-        else if (password != Lotfi.getpass(0) && Passcunter == 0) {
+        else if (password != Lotfi.getPass(0) && Passcunter == 0) {
             cout << "Wrong pass! Try another 30 min" << endl; // Need get time from system
             break;
         }
-        else if (password != Lotfi.getpass(0)) {
+        else if (password != Lotfi.getPass(0)) {
             cout << "Wrong pass! You have " << Passcunter << " try" << endl;
             cout << "Forget pass? (*hint* type \"Forget\" to check and change password)" << endl;
             Passcunter--;
         }
-        if (password == Lotfi.getpass(0)) {
+        if (password == Lotfi.getPass(0)) {
             cout << "Welcome " << Lotfi.getUser(0) << endl;
+            return true;
+        }
+    } while (Passcunter >= 0);
+    return false;
+}
+bool C_authenticateUser(Collegian& Student, int& Passcunter, string& username, string& password) {
+    int userIndex = -1;
+    do {
+        cout << "Enter Username: " << endl;
+        cin >> username;
+        userIndex = Student.findUser(username);
+        if (userIndex == -1) {
+            cout << "Username not found. Please try again." << endl;
+        }
+    } while (userIndex == -1);
+    do {
+        cout << "Enter Password: " << endl;
+        cin >> password;
+        if (password == "Forget") {
+            // First need to implement password change logic
+            cout << "Success! Password has been changed." << endl; // For testing
+            Passcunter = 3;
+        }else if (password != Student.getPass(userIndex) && Passcunter == 0) {
+            cout << "Wrong password! Try again in 30 minutes." << endl; // Need to get time from system
+            break;
+        }
+        else if (password != Student.getPass(userIndex)) {
+            cout << "Wrong password! You have " << Passcunter << " tries left." << endl;
+            cout << "Forgot password? (*hint* type \"Forget\" to check and change password)" << endl;
+            Passcunter--;
+        }
+        if (password == Student.getPass(userIndex)) {
+            cout << "Welcome " << username << endl;
             return true;
         }
     } while (Passcunter >= 0);
